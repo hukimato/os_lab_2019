0b1c30630151ac9f3a9e7787f46eef1c87487c40
hukimato <43462720+hukimato@users.noreply.github.com>
2021-04-21 15:45:32 +0300

    Create lib.h

---
+++ lab6/src/lib.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+struct FactorialArgs {
+  uint64_t begin;
+  uint64_t end;
+  uint64_t mod;
+};
+
+uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod);
